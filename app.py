89ca06a7b3b26d94c167c160067caf8d2aec2a40
Зезюлькин Сергей <zezyulkins@Belkanton.local>
2018-07-25 13:52:05 +0300

    v1

---
+++ app.py
@@ -0,0 +1,63 @@
+import utils
+import os
+from flask import Flask, request, jsonify
+from sklearn.externals import joblib
+
+
+app = Flask(__name__)
+
+clf = joblib.load('static/itil-multinb.pkl')
+transformer = joblib.load('static/itil-tfidf.pkl')
+transformer._validate_vocabulary()
+
+app.config['UPLOAD_FOLDER'] = 'data/'
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    j = request.json
+    s = utils.preprocess(j['title'])
+    features = transformer.transform([s])
+    prediction = clf.predict(features)
+    return jsonify({'prediction': str(prediction[0])})
+
+
+@app.route('/upload', methods=['GET', 'POST', 'DELETE'])
+def upload_file():
+
+    if request.method == 'POST':
+        f = request.files['file']
+        fname = os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
+        f.save(fname)
+        return f.filename
+
+    elif request.method == 'GET':
+        return jsonify(os.listdir(app.config['UPLOAD_FOLDER']))
+
+    elif request.method == 'DELETE':
+        j = request.json
+        removed = []
+        for f in j:
+            fname = os.path.join(app.config['UPLOAD_FOLDER'], f)
+            try:
+                os.remove(fname)
+                removed.append(f)
+            except:
+                pass
+        return jsonify(removed)
+
+
+@app.route('/train', methods=['POST'])
+def train():
+    j = request.json
+    fname = os.path.join(app.config['UPLOAD_FOLDER'], j['file_data'])
+    tfidf, model = utils.train(fname)
+
+    joblib.dump(tfidf, os.path.join('static/itil-tfidf.pkl'))
+    joblib.dump(model, os.path.join('static/itil-multinb.pkl'))
+
+    return 'success'
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ utils.py
@@ -0,0 +1,53 @@
+import re
+import pymorphy2
+import pandas as pd
+from pymorphy2.tokenizers import simple_word_tokenize
+from pymorphy2.shapes import is_punctuation
+from stop_words import get_stop_words
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+morph = pymorphy2.MorphAnalyzer()
+stop_words = get_stop_words('russian')
+
+def replace_by_re(s, pattern, repl):
+    return re.sub(pattern, repl, s)
+
+def lemmatize(s):
+    l = [morph.parse(w)[0].normal_form for w in simple_word_tokenize(s) if not is_punctuation(w)]
+    l = [w for w in l if w not in stop_words]
+    return " ".join(l)
+
+def preprocess(s):
+    s = re.sub('1[CСcс]', "1С", s)
+    s = re.sub('FW:\s+', "", s)
+    s = re.sub('RE:\s+', "", s)
+    s = re.sub('(\d{2}[\.\\\/]){2}(\d{2})+', "дата", s)
+    s = lemmatize(s)
+    return s
+
+def train(file_data):
+    df = pd.read_csv(file_data, sep=';')
+    df = df.dropna()
+
+    df['specialist'].replace(['Руководители службы Service Desk', 'Шкурупий Денис'], 'Манюхин Андрей', inplace=True)
+    df['specialist'].replace('Кухарчук Дмитрий', 'Сергиевич Юрий', inplace=True)
+    df['specialist'].replace('Талаева Вера', 'Кропис Юлия', inplace=True)
+    df['specialist'].replace('Быков Вадим', 'Полегошко Андрей', inplace=True)
+
+    grouped = df['specialist'].value_counts()
+    users_minor = grouped[grouped <= 10]
+
+    for um in users_minor.index:
+        df = df.loc[df['specialist'] != um]
+
+    df['title'] = df['title'].apply(lambda x: preprocess(x))
+
+    tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', ngram_range=(1, 2))
+    features = tfidf.fit_transform(df.title).toarray()
+    labels = df['specialist']
+
+    model = MultinomialNB()
+    model.fit(features, labels)
+
+    return tfidf, model
